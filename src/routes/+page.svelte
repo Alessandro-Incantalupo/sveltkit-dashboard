3e20549c80df1061af54a43d24d8ef113581033c
Alessandro-Incantalupo <alessandro.incantalupo@gmail.com>
2025-08-05 10:43:04 +0200

    feat: scaffolding first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-red-500">Welcome to SvelteKit</h1>
+<p class="text-blue-500">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+<button class="rounded-lg bg-red-500 px-4 py-2 text-white"> Test Tailwind Button </button>
